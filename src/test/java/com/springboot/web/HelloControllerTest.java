6ecdc4f39d19dfde1004cc1259c10c3693673bb1
Lee JaeGeun <ljg95924@github.com>
2022-03-14 11:19:44 +0900

    Test: helloDto 테스트

--- src/test/java/com/springboot/web/HelloControllerTest.java
+++ src/test/java/com/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
 // 여기서는 SpringRuner라는 스프링 실행자를 사용
@@ -39,5 +39,22 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 // 응답본문의 내용을 검증, Controllor에서 'hello'를 리턴하기 때문에 이값이 맞는지 검증
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        // 1. param
+        // API 테스트할때 사용될 요청 파라미터, 단 값은 String만 허용
+        // 2. jsonPath
+        // JSON 응답값을 필드별로 검증할 수 있는 메소드, $를 기준으로 필드명을 명시
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
