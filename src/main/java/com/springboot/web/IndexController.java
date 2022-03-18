a811d0ad2ea6ee4dc34ce07d8eaf0e2736a9cb58
Lee JaeGeun <ljg95924@github.com>
2022-03-18 10:38:21 +0900

    Refactor: 어느테이션 기반으로 코드개선

--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.web;
 
+import com.springboot.confing.auth.LoginUser;
 import com.springboot.confing.auth.dto.SessionUser;
 import com.springboot.service.posts.PostsService;
 import com.springboot.web.dto.PostsResponseDto;
@@ -23,18 +24,28 @@ public class IndexController {
         return "posts-save";
     }
 
+    /*    @GetMapping("/")
+        // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+        // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts 로 index.mustache 에 전달
+        public String index(Model model) {
+            model.addAttribute("posts", postsService.findAllDesc());
+    
+            // (SessionUser) httpSession.getAttribute("user"): CustomOAuth2UserService 에서 로그인 성공시 세션에 SessionUser 를 저장함
+            // 즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+            SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    
+            // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록
+            // 세션에 저장된 값이 없으면 model 에는 아무런 값이 없는 상태니 로그인 버튼이 보이게 됨
+            if (user != null) {
+                model.addAttribute("userName", user.getName());
+            }
+            return "index";
+        }*/
+    // @LoginUser SessionUser user: 기존에(User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선됨
+    // 이제는 어느 컨트롤러든지 @LoginUser 만 사용하면 세션 정보를 가져올 수 있게 됨
     @GetMapping("/")
-    // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
-    // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        // (SessionUser) httpSession.getAttribute("user"): CustomOAuth2UserService 에서 로그인 성공시 세션에 SessionUser 를 저장함
-        // 즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록
-        // 세션에 저장된 값이 없으면 model 에는 아무런 값이 없는 상태니 로그인 버튼이 보이게 됨
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
